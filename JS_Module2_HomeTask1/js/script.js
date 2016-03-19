c0c6cb11501d6815e1b5c740d3dc23b113a8dbe4
Roman Basaraba <manager@bastards.com.ua>
2016-03-19 21:55:32 +0200

    Added Some Changes To JS HomeTask 1

--- JS_Module2_HomeTask1/js/script.js
+++ JS_Module2_HomeTask1/js/script.js
@@ -5,9 +5,6 @@ function myPow(a, b) {
 var a = prompt('Введите число которое будете возводить в степень:', '');
 var b = prompt('Введите число для степени в которую будете возводить:', '');
 
-alert(Math.round(a));
-alert(Math.round(b));
-
 if ( !isNaN ( Math.round(a) ) ) {
     if ( !isNaN ( Math.round(b) ) ) {
         console.log('Result of POW = ', myPow(a, b) );
