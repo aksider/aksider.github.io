4d548fcf15caca1d0e8445ff40280f110dc13d29
Roman Basaraba <manager@bastards.com.ua>
2016-03-21 20:44:29 +0200

    Added Some Changes To JS HomeTask 1

--- JS_Module2_HomeTask1/js/script.js
+++ JS_Module2_HomeTask1/js/script.js
@@ -1,12 +1,14 @@
-function myPow(a, b) {
-    return (!b) ? 1 : a * myPow(a, b-1);
-}
-
 var a = prompt('Введите число которое будете возводить в степень:', '');
 var b = prompt('Введите число для степени в которую будете возводить:', '');
 
 if ( !isNaN ( Math.round(a) ) ) {
     if ( !isNaN ( Math.round(b) ) ) {
-        console.log('Result of POW = ', myPow(a, b) );
+        if ( b > 0) {
+            console.log('Result of POW = ', myPow(a, b) );
+        }
     }
+}
+
+function myPow(a, b) {
+    return (!b) ? 1 : a * myPow(a, b-1);
 }
