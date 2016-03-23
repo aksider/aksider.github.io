f0feaeadc0ff63b1ba4534b688b74e90224c3bab
Roman Basaraba <manager@bastards.com.ua>
2016-03-23 21:20:24 +0200

    Added Some Changes To JS HomeTask 1

--- JS_Module2_HomeTask1/js/script.js
+++ JS_Module2_HomeTask1/js/script.js
@@ -1,10 +1,26 @@
-var a = prompt('Введите число которое будете возводить в степень:', '');
-var b = prompt('Введите число для степени в которую будете возводить:', '');
+var a, b;
 
-if ( !isNaN ( Math.round(a) ) ) {
-    if ( !isNaN ( Math.round(b) ) ) {
-        if ( b > 0) {
+enterData();
+
+function enterData() {
+    a = prompt('Введите число которое будете возводить в степень:', '');
+    b = prompt('Введите число для степени в которую будете возводить:', '');
+    
+    checkInteger();
+}
+
+function isInteger(x, y) {
+  return (x == y);
+}
+
+function checkInteger() {
+    if (!isInteger(a, parseInt(a)) || !isInteger(b, parseInt(b)) ) {
+        enterData();
+    } else {
+        if ( b > 0 ) {
             console.log('Result of POW = ', myPow(a, b) );
+        } else {
+            console.log('Result of POW = ', 1/(myPow(a, -b)) );
         }
     }
 }
