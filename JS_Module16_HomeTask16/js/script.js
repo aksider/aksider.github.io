8fe2f081ae96c50214a510f6668dc7224911ad99
Roman Basaraba <manager@bastards.com.ua>
2016-05-02 15:29:45 +0300

    Added Some Chainges to JS HomeTask 16 Files

--- JS_Module16_HomeTask16/js/script.js
+++ JS_Module16_HomeTask16/js/script.js
@@ -1,66 +1,97 @@
-var animal = {
-    name: 'Jack',
-    age: 2
-};
-
-var cat = {
-//    name: 'Vasya',
-    age: 3
-};
-
-cat.__proto__ = animal;
-
-console.log('cat.name', cat.name);
-
-
-function World() {
-    this.sky = 'blue';
+function Human() {
+    this.name = 'name';
+    this.age = 0;
+    this.sex = 'sex';
+    this.height = 0;
+    this.weight = 0;
 }
 
-
-//var newWorld = new World();
-
-function Company() {
-    this.name = 'Google';
-    this.people = 10000;
-    this.sayHello = function () {
-        alert('Hello, World!');
-    }
-}
-
-
-//Company.prototype = newWorld;
-Company.prototype = new World();
-
-
-var newCompany = new Company();
-
-console.log('newCompany', newCompany);
-console.log('newCompany.sky', newCompany.sky);
-
-newCompany.sayHello();
-
-
-Company.prototype.sayHello2 = function () {
-    alert('Hello, World 2!!!');
-}
-
-var newCompany2 = new Company();
-
-newCompany2.sayHello2();
-
-
-var Constructor = function(){};
-var object = new Constructor();
-console.log(object.__proto__ === Constructor);
-console.log(object.__proto__ === Constructor.prototype);
-console.log(object.__proto__ === Constructor.__proto__);
-
-var Constructor4 = function() {
-        this.a = 1;
-        this.b = 2;
+function Worker() {
+    this.workingPlace = 'workingPlace';
+    this.salary = 0;
+    this.toWork = function () {
+        console.log('I am working');
     };
+}
 
-var object2 = Constructor4();
+function Student() {
+    this.studyingPlace = 'studyingPlace';
+    this.grants = 0;
+    this.watchSoaps = function () {
+        console.log('I am watching soaps');
+    };
+}
 
-console.log(object2);
+Worker.prototype = new Human();
+Student.prototype = new Human();
+
+var Worker1 = new Worker();
+var Worker2 = new Worker();
+var Worker3 = new Worker();
+var Student1 = new Student();
+var Student2 = new Student();
+var Student3 = new Student();
+
+
+Worker1.name = 'John Smith';
+Worker1.age = 30;
+Worker1.sex = 'male';
+Worker1.height = 180;
+Worker1.weight = 90;
+Worker1.workingPlace = 'CIA';
+Worker1.salary = 30000;
+Worker1.toWork;
+
+Worker2.name = 'Peter Smith';
+Worker2.age = 45;
+Worker2.sex = 'male';
+Worker2.height = 175;
+Worker2.weight = 85;
+Worker2.workingPlace = 'KIA';
+Worker2.salary = 3000;
+Worker2.toWork;
+
+Worker3.name = 'Julia Jackson';
+Worker3.age = 33;
+Worker3.sex = 'female';
+Worker3.height = 160;
+Worker3.weight = 60;
+Worker3.workingPlace = 'Google';
+Worker3.salary = 5000;
+Worker3.toWork;
+
+Student1.name = 'Ivan Ivanov';
+Student1.age = 20;
+Student1.sex = 'male';
+Student1.height = 170;
+Student1.weight = 60;
+Student1.studyingPlace = 'Univer';
+Student1.grants = 1000;
+Student1.watchSoaps;
+
+Student2.name = 'Maria Kukushkina';
+Student2.age = 21;
+Student2.sex = 'female';
+Student2.height = 160;
+Student2.weight = 55;
+Student2.studyingPlace = 'Univer2';
+Student2.grants = 900;
+Student2.watchSoaps;
+
+Student3.name = 'Kolya Sidorov';
+Student3.age = 22;
+Student3.sex = 'male';
+Student3.height = 175;
+Student3.weight = 80;
+Student3.studyingPlace = 'Univer3';
+Student3.grants = 1200;
+Student3.watchSoaps;
+
+
+console.log(Worker1);
+console.log(Worker2);
+console.log(Worker3);
+
+console.log(Student1);
+console.log(Student2);
+console.log(Student3);
