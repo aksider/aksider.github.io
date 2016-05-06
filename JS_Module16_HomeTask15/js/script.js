634a342437d71fb3ee39b0ae8cf94bdd12514ef3
Roman Basaraba <manager@bastards.com.ua>
2016-05-06 23:04:33 +0300

    Added Some Chainges to JS HomeTask 15 Files

--- JS_Module16_HomeTask15/js/script.js
+++ JS_Module16_HomeTask15/js/script.js
@@ -1,11 +1,11 @@
-function GoogleCallBack(jQueryObj, data) {
-//function GoogleCallBack(object) {
+//function GoogleCallBack(jQueryObj, data) {
+function GoogleCallBack(object) {
 
     var $body = $('body');
     var $content;
     
-//    console.log('data', object);
-    console.log('data', data);
+    console.log('data', object);
+//    console.log('data', data);
     
     for(var i = 0; i < data.results.length; i++) {
         
@@ -28,7 +28,7 @@ function searchGoogle() {
 
 //last worked       //        url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=' + $('#searchText').val() + '&callback=GoogleCallBack&context=?',
 //        url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDgNjaKajvyzbI6uZlqOsa_HY6Th3w5Gfk&q=' + $('#searchText').val() + '&callback=GoogleCallBack&context=?',
-        url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDgNjaKajvyzbI6uZlqOsa_HY6Th3w5Gfk&cx=011168767613474021112:o098zgkqbg4&q=' + $('#searchText').val() + '&callback=GoogleCallBack',
+        url: 'https://www.googleapis.com/customsearch/v1?key=AIzaSyDgNjaKajvyzbI6uZlqOsa_HY6Th3w5Gfk&cx=011168767613474021112:o098zgkqbg4&q=' + $('#searchText').val() + '&callback=GoogleCallBack&context=?',
 //        url: 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&q=' + 'test' + '&callback=GoogleCallBack&context=?',
         data: {
             abc: 123
